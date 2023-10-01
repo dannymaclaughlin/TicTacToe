bbaba6c13f8f68d1e21e6c4224f0bb3e7c136a15
dannymaclaughlin <dannymaclaughlin@gmail.com>
2023-09-30 22:33:08 -0700

    Created initializeGameboard function and created two player objects

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const gameboard = (function() {
     const gameboard = [];
+
+    (function initializeGameboard() {
+        for(i=0; i<9; i++) {
+            const boardSpaces = document.createElement('div');
+            boardSpaces.className = 'boardSpaces';
+            document.getElementById('gameboard').appendChild(boardSpaces);
+        }
+    })();
+
 })();
 
 const player = (name) => {
-
+    return { name };
 };
+
+let player1 = player('player 1');
+let player2 = player('player 2');
 
 const game = {
 
