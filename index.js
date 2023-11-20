788a5c52d61e61b9c76523045b52d5a3c3932528
dannymaclaughlin <dannymaclaughlin@gmail.com>
2023-11-20 10:17:44 -0800

    Restarted JS file from a clean slate in test branch. So far I have added the gameboard, players, and gameController objects as well as the beginning of the cell function.

--- index.js
+++ index.js
@@ -1,84 +1,30 @@
-// GAMEBOARD OBJECT
-const gameboardObject = (function () {
-    let gameboardArray = ['', '', '', '', '', '', '', '', ''];
+const gameboard = (function () {
+    const board = ['', '', '', '', '', '', '', '', ''];
 
-	function renderGameboard() {
-        let gameboard = document.getElementById('gameboard');
-        gameboard.innerHTML = '';
-        gameboardSpaces = [];
-
-		// create a loop that will iterate over each item in the gameboardArray
-        for (let i = 0; i < gameboardArray.length; i++) {
-            let space = document.createElement('div');
-
-            space.setAttribute('id', `boardspace-${i}`);
-            space.setAttribute('class', 'boardSpaces');
-            space.textContent = gameboardArray[i];
-            
-            gameboard.appendChild(space);
-            gameboardSpaces.push(space);
-        };
-
-        selectSpace();
-        return gameboardSpaces;
-	};
-
-    // renders gameboard & assigns it to a variable for future use
-    gameboardSpaces = renderGameboard();
-    
-	function selectSpace() {
-        for (let i = 0; i < gameboardSpaces.length; i++) {
-            let space = gameboardSpaces[i];
-
-            space.addEventListener('click', () => {
-                markSpace(space, i);
-            });
-        };
+    for (let i = 0; i < board.length; i++) {
+        board[i] = [];
     };
-    
-    selectSpace();
-    
-	function markSpace(space, index) {
-        gameboardArray[index] = 'X';
-        renderGameboard();
-        selectSpace();
-        console.log(gameboardObject.gameboardArray);
-	};
-
-	function checkSpaceValidation() {
-		// create an if...else statement to check if the selected space's innerHTML is === to a player's symbol
-			// if the space has no mark call markSpace()
-			// else return — do not mark the space
-	};
-
-    function updateBoard() {
-
-    }
-
-    return { 
-        gameboardArray,
-        renderGameboard,
-        markSpace,
-        updateBoard,
-        selectSpace,
-    };
-    
 })();
 
-// PLAYER OBJECTS
-const playerObjects = {
-	player1: {
-		name: 'Player 1',
-		symbol: 'X',
-	},
+function cell() {
+    let value = '';
 
-	player2: {
-        name: 'Player 2',
-        symbol: 'O',
-	},
+    const addMark = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        addMark,
+        getValue,
+    };
 };
 
-// GAME FLOW OBJECTS
+const players = {
+
+};
+
 const gameController = (function () {
 
 })();
