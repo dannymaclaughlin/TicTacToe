69726de4456cc658748e8ed0d99a1f940884d500
dannymaclaughlin <dannymaclaughlin@gmail.com>
2024-02-03 13:00:53 -0800

    Restarted javascript code from scratch. Gameboard is stored as an array inside of a gameboard object.

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
 // GAMEBOARD OBJECT
 const gameboard = (function () {
     const board = ['', '', '', '', '', '', '', '', ''];
-
-    const boardspaces = document.querySelectorAll('.boardspace');
-
-    boardspaces.forEach((space, index) => {
-        board[index] = space;
-        
-        space.addEventListener('click', () => {
-            console.log(`clicked on boardspace${index + 1}`);
-            space.innerHTML = 'X'
-            
-            // use these to target specific spaces
-                // console.log(space);
-                // console.log(board[2]);
-        })
-    })
 })();
-
-// PLAYERS OBJECT
-
-// GAMECONTROLLER OBJECT
-const gameController = (function () {
- 
-})();
-
-// initialize game
-// gameController.startGame();
