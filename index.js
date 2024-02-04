58776dd41ccd1003e7faba86319b1f02adb7e9ef
dannymaclaughlin <dannymaclaughlin@gmail.com>
2024-02-03 20:39:20 -0800

    Created player and game objects.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-// GAMEBOARD OBJECT
+// GAMEBOARD OBJECT (IIFE)
 const gameboard = (function () {
     const board = ['', '', '', '', '', '', '', '', ''];
+})();
+
+// PLAYER OBJECT (FACTORY FUNCTION)
+function createPlayer (name, symbol) {
+    const playerName = name;
+    const playerSymbol = symbol;
+    return { playerName, playerSymbol };
+}
+
+// GAME OBJECT (IIFE)
+const game = (function () {
+
 })();
