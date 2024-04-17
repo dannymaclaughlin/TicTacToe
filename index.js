b0a5255b8a0249dd742a560dedbe75f7f63646f3
dannymaclaughlin <dannymaclaughlin@gmail.com>
2024-04-16 18:07:53 -0700

    Created a player factory function and player 1 & 2 Also returned board in the Gameboard object.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-const gameboard = (function () {
+const Gameboard = (function () {
     const board = [
         '', '', '', 
         '', '', '', 
         '', '', '',
     ];
+
+    return { board }
 })();
+
+function createPlayer (username, symbol) {
+    return { username, symbol }
+};
+
+const player1 = createPlayer('Player 1', 'X');
+const player2 = createPlayer('Player 2', 'X');
