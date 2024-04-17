e192b411d53182eb9a8307ce92bdbb52cca6abb6
dannymaclaughlin <dannymaclaughlin@gmail.com>
2024-04-16 18:29:05 -0700

    Added comments to help visually seperate sections

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// GAMEBOARD OBJECT - DIRECTLY RELATED TO THE BOARD
 const Gameboard = (function () {
     const board = [
         '', '', '', 
@@ -8,9 +9,15 @@ const Gameboard = (function () {
     return { board }
 })();
 
+// PLAYER OBJECTS
 function createPlayer (username, symbol) {
     return { username, symbol }
 };
 
 const player1 = createPlayer('Player 1', 'X');
 const player2 = createPlayer('Player 2', 'O');
+
+// GAMECONTROLLER - OBJECT TO CONTROL THE FLOW OF THE GAME
+function Gamecontroller() {
+
+};
