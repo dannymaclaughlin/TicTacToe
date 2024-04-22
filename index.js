57f2f0903d494ff9a764b86f7b45a2cc4052e604
dannymaclaughlin <dannymaclaughlin@gmail.com>
2024-04-21 23:40:56 -0700

    Added logic to verify if a space is occupied, and calls on setSpace method to mark space if space is available.

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ const Gameboard = (function () {
     }
 
     function setSpace(row, column, symbol) {
-        board[row][column] = symbol;
+        if (board[row][column] === '') {
+            board[row][column] = symbol;
+            return true; // space was successfully marked
+        } else {
+            return false; // space is occupied
+        }
     }
 
     return { getBoard, setSpace }
@@ -55,11 +60,25 @@ const GameController = (function () {
 
     function selectSpace(row, column) {
         // logic to select a space
-        
+        isSpaceEmpty(row, column);
+
+        const symbol = activePlayer.symbol;
+        if (Gameboard.setSpace(row, column, symbol)) {
+            console.log(`${activePlayer.name} has selected space (${row}, ${column})`);
+            switchPlayerTurn();
+            checkForTie();
+            checkForWinner();
+        } else {
+            console.log('Invalid move. Please select another space.')
+        }
     }
 
-    function isSpaceEmpty() {
+    function isSpaceEmpty(row, column) {
         // logic to verify if space is occupied
+        if (Gameboard.getBoard()[row][column] !== '') {
+            console.log('Selected space is already occupied. Please select another space.')
+            return;
+        }
     }
 
     function markSpace() {
@@ -78,7 +97,7 @@ const GameController = (function () {
         // logic to end a game
     }
 
-    return { startGame, activePlayer, getPlayer, switchPlayerTurn };
+    return { startGame, activePlayer, getPlayer, switchPlayerTurn, selectSpace };
 })();
 
 GameController.startGame();
